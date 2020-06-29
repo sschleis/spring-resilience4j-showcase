77b4904a73e08a575c3f754d8d68fffa397c347b
sschleis <sebastian.schleis@gmail.com>
2020-06-29 21:58:53 +0200

    Update to new Spring Boot Version 2.3.1 & Kotlin 1.3.70

--- src/test/kotlin/de/sschleis/showcase/springresilience4jshowcase/CBTest.kt
+++ src/test/kotlin/de/sschleis/showcase/springresilience4jshowcase/CBTest.kt
@@ -1,6 +1,5 @@
 package de.sschleis.showcase.springresilience4jshowcase
 
-import de.sschleis.showcase.springresilience4jshowcase.CBShowcase
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.springframework.beans.factory.annotation.Autowired
